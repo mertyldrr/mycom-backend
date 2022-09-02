359f5183be0c08c187e920dfe754a3942a3f61fc
mertyldrr <mertyildirir14@gmail.com>
2022-09-02 05:37:53 +0300

    nestjs config(dotenv) imported

--- src/photo/photo.module.ts
+++ src/photo/photo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PhotoController } from './photo.controller';
 import { PhotoService } from './photo.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   controllers: [PhotoController],
   providers: [PhotoService],
 })
