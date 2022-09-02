bbfd6ea0b814808a45c4e0ab1492cbc8ad3e63e5
mertyldrr <mertyildirir14@gmail.com>
2022-09-02 05:41:23 +0300

    fetching presigned photo urls from s3 bucket

--- src/photo/photo.controller.ts
+++ src/photo/photo.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { PhotoService } from './photo.service';
 
 @Controller()
 export class PhotoController {
   constructor(private photoService: PhotoService) {}
   @Get('photos')
-  getPhotos() {
-    return this.photoService.getPhotos();
+  async getPhotos(@Req() req: Request, @Res() res: Response) {
+    const photos = await this.photoService.getPhotos();
+    if (photos) {
+      res.send(photos);
+    } else {
+      res.status(400).send({ msg: 'Photo urls not found' });
+    }
   }
 }
--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
-
+import {
+  S3Client,
+  GetObjectCommand,
+  ListObjectsCommand,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+console.log('aa');
 @Injectable({})
 export class PhotoService {
-  getPhotos() {
-    return 'Photos fetched';
+  s3BucketName = process.env.S3_BUCKET_NAME;
+  s3BucketRegion = process.env.S3_BUCKET_REGION;
+  accessKey = process.env.AWS_ACCESS_KEY_ID;
+  secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  s3Client = new S3Client({
+    credentials: {
+      accessKeyId: this.accessKey,
+      secretAccessKey: this.secretAccessKey,
+    },
+    region: this.s3BucketRegion,
+  });
+  async getPhotos() {
+    const listParams = {
+      Bucket: this.s3BucketName,
+    };
+    const {
+      Name,
+      $metadata: response,
+      Contents: images,
+    } = await this.s3Client.send(new ListObjectsCommand(listParams));
+    if (response.httpStatusCode === 200) {
+      const imageUrls = [];
+      for (const image of images) {
+        const obj = {};
+        const signedUrlParams = {
+          Bucket: Name,
+          Key: image.Key,
+        };
+        const getObjectCommand = new GetObjectCommand(signedUrlParams);
+        const url = await getSignedUrl(this.s3Client, getObjectCommand, {
+          expiresIn: 60,
+        });
+        obj['url'] = url;
+        imageUrls.push(obj);
+      }
+      return JSON.stringify(imageUrls);
+    }
   }
 }
