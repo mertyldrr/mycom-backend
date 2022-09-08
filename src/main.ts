36af349fab569a0a12f7359ffab1dd67181ec31c
mertyldrr <mertyildirir14@gmail.com>
2022-09-08 08:44:29 +0300

    console.log triggers after server run successfully

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(`Server listening on port ${process.env.PORT}`);
   await app.listen(process.env.PORT);
+  console.log(`Server listening on port ${process.env.PORT}`);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
