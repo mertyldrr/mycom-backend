d193504e7ea091ae52c334fbcf82160e06ce9093
mertyldrr <mertyildirir14@gmail.com>
2023-04-27 20:05:00 +0200

    create separate functions for photos and icons

---
+++ src/photo/media.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { mediaService } from './media.service';
+
+@Controller()
+export class MediaController {
+  constructor(private mediaService: mediaService) {}
+  @Get()
+  sayHello() {
+    return `Hello World`;
+  }
+  @Get('icons')
+  async getIcons(@Req() req: Request, @Res() res: Response) {
+    const icons = await this.mediaService.getIcons();
+    if (icons) {
+      /*
+       * res.json eventually calls res.send but also ensures the response will have utf-8 charset and application/json Content-Type
+       */
+      res.json(icons);
+    } else {
+      res.status(400).json({ msg: 'Icons are not found' });
+    }
+  }
+  @Get('photos')
+  async getPhotos(@Req() req: Request, @Res() res: Response) {
+    const photos = await this.mediaService.getPhotos();
+    if (photos) {
+      /*
+       * res.json eventually calls res.send but also ensures the response will have utf-8 charset and application/json Content-Type
+       */
+      res.json(photos);
+    } else {
+      res.status(400).json({ msg: 'Icons are not found' });
+    }
+  }
+}
