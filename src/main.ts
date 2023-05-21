928da76fea6035773e9d1723c4ae4e5fd72e553d
mertyldrr <mertyildirir14@gmail.com>
2023-05-21 17:40:20 +0200

    fix news endpoint

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 declare const module: any;
+
+const logger = new Logger('Server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT);
-  console.log(`Server listening on port ${process.env.PORT}`);
+
+  logger.log(`Server listening on port ${process.env.PORT}`);
+
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
--- src/win11/news.controller.ts
+++ src/win11/news.controller.ts
@@ -1,11 +1,12 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
-import { Controller, Get, Inject, Req, Res } from '@nestjs/common';
+import { Controller, Get, Inject, Logger, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { NewsService } from './news.service';
 
 @Controller()
 export class NewsController {
+  private readonly logger = new Logger(NewsService.name);
   constructor(
     private newsService: NewsService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
@@ -18,14 +19,18 @@ export class NewsController {
     const cachedNews = await this.cacheManager.get(cacheKey);
 
     if (cachedNews) {
-      console.log('news cached');
+      this.logger.log('news cached');
       // Data is found in the cache, return it
-      return cachedNews;
+      return res.json(cachedNews);
     } else {
       const news = await this.newsService.getNews();
       if (news) {
-        res.json(news);
-        await this.cacheManager.set('news', news, 43200);
+        await this.cacheManager.set(
+          'news',
+          news,
+          parseInt(process.env.CACHE_TTL_IN_MS),
+        );
+        return res.json(news);
       } else {
         res.status(400).json({ msg: 'News are not found' });
       }
