d67f333808927bb87df2b43bb7dbe3e1b3e8a5bf
mertyldrr <mertyildirir14@gmail.com>
2023-06-04 01:57:45 +0200

    add auth and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { MediaModule } from './mycom/media/media.module';
-import { NewsModule } from './win11/news.module';
+import { AuthModule } from './reddit/auth/auth.module';
+import { UserModule } from './reddit/user/user.module';
+import { NewsModule } from './win11/news/news.module';
 
 @Module({
   imports: [
     MediaModule,
     NewsModule,
+    AuthModule,
+    UserModule,
     ThrottlerModule.forRoot({ ttl: 86400, limit: 1000 }),
   ],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
