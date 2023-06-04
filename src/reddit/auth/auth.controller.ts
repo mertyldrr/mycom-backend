5679b3db361fe5c76880c6bf30586de589d4c220
mertyldrr <mertyildirir14@gmail.com>
2023-06-05 01:15:08 +0200

    add signup logic[wip]

--- src/reddit/auth/auth.controller.ts
+++ src/reddit/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   // POST /auth/signup
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      dto,
+    });
+    return this.authService.signup(dto);
   }
   // POST /auth/login
   @Post('login')
--- src/reddit/auth/auth.service.ts
+++ src/reddit/auth/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'Signup successfull' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hashPw: hash,
+      },
+    });
+    // return the saved user
+    return user;
   }
 
   login() {
