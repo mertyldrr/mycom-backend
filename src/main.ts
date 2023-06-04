3506505dce335a6d52c02842e109e3d5b0c9294c
mertyldrr <mertyildirir14@gmail.com>
2023-06-05 00:46:21 +0200

    add class-transformer, class-validator and argon2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 declare const module: any;
 
@@ -11,6 +11,7 @@ const logger = new Logger('Server');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT);
 
   logger.log(`Server listening on port ${process.env.PORT}`);
