1239412590f556d6065543c083d4d23c31812aeb
mertyldrr <mertyildirir14@gmail.com>
2025-05-04 22:21:28 +0300

    chore(*): remove reddit app and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,13 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { MediaModule } from './mycom/media/media.module';
-import { AuthModule } from './reddit/auth/auth.module';
-import { UserModule } from './reddit/user/user.module';
 import { NewsModule } from './win11/news/news.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     MediaModule,
     NewsModule,
-    AuthModule,
-    UserModule,
     ThrottlerModule.forRoot({ ttl: 86400, limit: 1000 }),
-    PrismaModule,
   ],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
